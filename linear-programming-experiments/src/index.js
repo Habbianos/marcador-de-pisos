c1e5a89f800c026c91773b679de3d771984faf73
otavs <otavio.cts@gmail.com>
2020-08-21 21:28:08 -0300

    Refactor
    Remove unused import

--- linear-programming-experiments/src/index.js
+++ linear-programming-experiments/src/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
-const path = require('path')
 const os = require('os')
 const {exec} = require('child_process')
 const builder = require('./mapManager')
